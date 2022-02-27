ca4b8ddc53587792c24d1b8ab44183d6229f3cff
Diego Rodrigues <diegokod@gmail.com>
2022-02-26 21:49:24 -0300

    Aplicaçao parcialmente finalizada

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPLugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -7,32 +8,40 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
         mode: isDevelopment ? 'development':'production',
         devtool: isDevelopment ? 'eval-source-map':'source-map',
-        entry: path.resolve(__dirname, 'src', 'index.jsx'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'bundle.js',
     },
     resolve : {
-        extensions: ['.js', '.jsx', '.css'],
+        extensions: ['.js', '.jsx', '.css', '.ts', '.tsx'],
     },   
     devServer: {
-        // contentBase: path.resolve(__dirname, 'public'),
-        // contentBase Webpack Version v4
-        static: {
-            directory: path.resolve(__dirname, 'public'),
-        }
-    },
+        static : {
+        directory: path.resolve(__dirname, "public")
+        },
+        hot: true
+        },
+        
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPLugin(),
         new HtmlWebpackPlugin({
             template:path.resolve(__dirname, 'public', 'index.html')
         })
-        ],
+        ].filter(Boolean),
         module:  {
         rules: [
             {
-            test: /\.jsx$/,
+            test: /\.(j|t)sx$/,
             exclude: /node_modules/,
-            use: 'babel-loader',
+            use: {
+                loader: 'babel-loader', 
+                options: {
+                    plugins: [
+                        isDevelopment && require.resolve('react-refresh/babel')
+                    ].filter(Boolean)
+                }
+            },
             },
             {
             test: /\.scss$/,
